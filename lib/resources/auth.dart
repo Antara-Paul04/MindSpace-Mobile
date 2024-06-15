cd88ff31cf225d30040e4fa5cdef5fb7bbb10ccd
Subroto <subroto.2003@gmail.com>
2024-06-16 05:10:32 +0530

    firebase added and auth code incomplete

---
+++ lib/resources/auth.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<String> signupUser(
+      {required String email,
+      required String password,
+      required String userName}) async {
+    String res = 'Some error occurred';
+    try {
+      if (email.isNotEmpty || password.isNotEmpty || userName.isNotEmpty) {
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+        print(cred.user!.uid);
+
+        await _firestore
+            .collection('user')
+            .doc(cred.user!.uid)
+            .set({'email': email, 'password': password, 'username': userName});
+
+        res = 'Success';
+      }
+    } on FirebaseAuthException catch (err) {
+      if (err.code == 'invalid-email') {
+        res = 'The email is badly formatted.';
+      } else if (err.code == 'weak-password') {
+        res = 'The password should be atleast 6 characters';
+      }
+    } catch (e) {
+      res = e.toString();
+    }
+    return res;
+  }
+}
