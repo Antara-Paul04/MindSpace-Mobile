03ecdec281fe28a3b33ac8bda2acac3e09567fce
Antara Paul <hiiamantara04@gmail.com>
2024-06-17 03:51:21 +0530

    feat: Login screen auth (incomplete)

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -199,7 +199,7 @@ class _SignupState extends State<Signup> {
                             child: Center(
                               child: Text('Sign up',
                                   style: GoogleFonts.lexend(
-                                      fontSize: 25, color: Colors.white)),
+                                      fontSize: 22, color: Colors.white)),
                             ),
                           ),
                   ),
