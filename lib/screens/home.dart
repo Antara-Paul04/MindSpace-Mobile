99b272e9ac83d5b3f924610b6313cf7f70bc222c
Subroto <subroto.2003@gmail.com>
2024-06-17 02:19:00 +0530

    feat: home screen user welcome text

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:ms/utils/colors.dart';
 
@@ -23,16 +24,31 @@ class HomeScreen extends StatelessWidget {
             radius: 26,
             backgroundColor: strokeColour,
             child: CircleAvatar(
-                  backgroundColor: Colors.white,
-                  radius: 25,
-                  child: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.notifications_none_rounded,
-                            color: primaryColour, size: 30,)),
+              backgroundColor: Colors.white,
+              radius: 25,
+              child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.notifications_none_rounded,
+                    color: primaryColour,
+                    size: 30,
+                  )),
             ),
           ),
         ],
         backgroundColor: bgColour,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(18),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Align(alignment: Alignment.topLeft,child: Text('Good Morning,',style: GoogleFonts.lexend(fontSize: 25,fontWeight: FontWeight.w500, color: Colors.black,height: 1))),
+              Align(alignment: Alignment.topLeft,child: Text('Antara!', style: GoogleFonts.lexend(fontSize: 25,fontWeight: FontWeight.w500, color: primaryColour))),
+            ],
+          ),
+        ),
       ),
     );
   }
